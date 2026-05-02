c6c796d01e62e64e28d5f56dec78e35a75e695e1
Shaun Parkison <shparkison@gmail.com>
2026-05-02 12:51:40 -0600

    version bump for release! 🎉

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
 
 # Application version
-VERSION = "0.4.7"
+VERSION = "0.4.8"
 
 
 class Settings(BaseSettings):
